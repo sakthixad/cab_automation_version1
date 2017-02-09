dd29226e73634fb8533bde30b3b4b69e185e0693
sakthixad <sakthi.gurumaharaj@xad.com>
2017-02-09 11:01:09 -0800

    Modified the config

--- config.py
+++ config.py
@@ -3,14 +3,14 @@ import sys
 
 API_URL_dev = "http://ec2-52-206-132-150.compute-1.amazonaws.com:8050"
 
-# if sys.argv[2] == "prod":
-#  API_URL= "http://ec2-54-152-88-4.compute-1.amazonaws.com:8050"
-#
-# if sys.argv[2] == "qa" or sys.argv[2] is None:
-#  API_URL= "http://ec2-52-38-47-137.us-west-2.compute.amazonaws.com:8050"
+if sys.argv[2] == "prod":
+ API_URL= "http://ec2-54-152-88-4.compute-1.amazonaws.com:8050"
+
+if sys.argv[2] == "qa" or sys.argv[2] is None:
+ API_URL= "http://ec2-52-38-47-137.us-west-2.compute.amazonaws.com:8050"
 
 
-API_URL= "http://ec2-52-38-47-137.us-west-2.compute.amazonaws.com:8050"
+# API_URL= "http://ec2-52-38-47-137.us-west-2.compute.amazonaws.com:8050"
 
 # API_URL= "http://ec2-54-152-88-4.compute-1.amazonaws.com:8050"
 
